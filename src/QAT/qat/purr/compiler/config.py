13b0931904a4ed3760ec51e74157bbd8cfa66ee7
Kajsa Eriksson Rosenqvist <keriksson.rosenqvist@oxfordquantumcircuits.com>
2023-10-16 09:29:37 +0100

    Old config and serializer path links for backwards compatibility.

---
+++ src/QAT/qat/purr/compiler/config.py
@@ -0,0 +1,4 @@
+from warnings import warn
+from qat_config.config import *
+warn(f"Deprecated config version qat-compiler v.1.0.0 internal "
+     "found, update package version.", DeprecationWarning)
---
+++ src/QAT/qat/purr/utils/serializer.py
@@ -0,0 +1,5 @@
+from warnings import warn
+from qat_config.serializer import _get_type
+from qat_config.serializer import *
+warn(f"Deprecated serializer version qat-compiler v.1.0.0 internal "
+     "found, update package version.", DeprecationWarning)
