c00c64c3d1b94fd72c2b5cb985e4be002cb5b65b
Kajsa Eriksson Rosenqvist <keriksson.rosenqvist@oxfordquantumcircuits.com>
2023-11-06 10:30:03 +0000

    Moved partial definitions.

--- tools/partial_package_builder.py
+++ tools/partial_package_builder.py
@@ -1,26 +1,32 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Oxford Quantum Circuits Ltd
 import argparse
-import toml
-
 from pathlib import Path
 from shutil import copy2, copytree
 from subprocess import run as run_cmd
 from tempfile import TemporaryDirectory
 
-parser = argparse.ArgumentParser("Partial Package builder", description="Easily package parts of the source code into a separate distribution.")
-parser.add_argument("-toml-file", required=True, help="The pyproject.toml file to use for the patial package.")
+import toml
+
+parser = argparse.ArgumentParser(
+    "Partial Package builder",
+    description="Easily package parts of the source code into a separate distribution."
+)
+parser.add_argument(
+    "--toml-file",
+    required=True,
+    help="The pyproject.toml file to use for the patial package."
+)
 
 
 class PartialPackageBuilder:
-
     def __init__(self, toml_file: str):
-        self.toml_path = Path(toml_file)
+        self.file_dir = Path(toml_file).parent
         self.toml = toml.load(toml_file)
-        self.package_dict = {p["include"]:p for p in
-                             self.toml["tool"]["poetry"]["packages"]}
+        self.package_dict = {
+            p["include"]: p for p in self.toml["tool"]["poetry"]["packages"]
+        }
         self.include_paths = self.toml["tool"]["poetry"].pop("package_includes")
-
 
     def build_partial_package(self):
         with TemporaryDirectory() as tempdir:
@@ -28,7 +34,14 @@ class PartialPackageBuilder:
                 self._include_path(path_string, tempdir)
             readme = self.toml["tool"]["poetry"].get("readme", None)
             if readme is not None:
-                copy2(readme, Path(tempdir, readme))
+                README = f"README{Path(readme).suffix}"
+                copy2(Path(self.file_dir, readme), Path(tempdir, README))
+                self.toml["tool"]["poetry"]["readme"] = README
+            self.toml["tool"]["poetry"]["version"] = run_cmd([
+                "poetry", "version", "--short"
+            ],
+                                                             capture_output=True,
+                                                             text=True).stdout.rstrip("\n")
             with open(Path(tempdir, "pyproject.toml"), "w+") as pyproject_file:
                 toml.dump(self.toml, pyproject_file)
             run_cmd(["poetry", "build"], cwd=tempdir)
@@ -38,7 +51,9 @@ class PartialPackageBuilder:
         rel_path = Path(path_string)
         pkg = self.package_dict.get(rel_path.parts[0], None)
         if pkg is None:
-            raise ValueError(f"Include path '{path_string}' is not part of any included package.")
+            raise ValueError(
+                f"Include path '{path_string}' is not part of any included package."
+            )
         from_path = Path(pkg.get("from", "."))
         path = Path(from_path, rel_path)
         self._build_parent_tree(path, from_path, tempdir)
