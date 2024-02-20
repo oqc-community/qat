c1ce043d4fe7229ec472dbaf0e89bf1bebbb6775
jamie <jfriel@oxfordquantumcircuits.com>
2024-02-20 21:42:30 +0000

    Add format code scripts into poetry

--- poetry_scripts.py
+++ poetry_scripts.py
@@ -6,3 +6,9 @@ def build_docs():
         'poetry run sphinx-apidoc -f -d 12 -M -e -o docs/sphinx/source/api/generated src/QAT "**/tests/**"'
     )
     os.system("poetry run sphinx-build docs/sphinx/source docs/sphinx/build")
+
+
+def format_code():
+    os.system("poetry run black .")
+    os.system("poetry run isort .")
+    os.system("poetry run autoflake .")
