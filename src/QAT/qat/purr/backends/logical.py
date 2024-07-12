475eb0d7d3fd1dc0a079c7c975a45dba951f38ad
jamie <jfriel@oxfordquantumcircuits.com>
2024-07-12 02:32:54 +0100

    getting logical circuit working for testing

--- src/QAT/qat/purr/backends/logical.py
+++ src/QAT/qat/purr/backends/logical.py
@@ -4,7 +4,8 @@ import math
 from typing import Any, List, Optional, Set, Union
 from enum import Enum, auto
 
-from qat.purr.compiler.builders import Axis, InstructionBuilder
+from qat.purr.backends.qiskit_simulator import QiskitHardwareModel
+from qat.purr.compiler.builders import Axis, FluidBuilderWrapper, InstructionBuilder, QuantumInstructionBuilder
 from qat.purr.compiler.config import InlineResultsProcessing
 from qat.purr.compiler.instructions import Acquire, ProcessAxis, ResultsProcessing
 from qat.purr.compiler.runtime import get_builder
@@ -54,24 +55,27 @@ class LogicalGate:
     def __str__(self):
         return f"{self.gate}[{self.qubit},{self.target_qubit} , {self.angle}]"
 
-class LogicalHardware():
+class LogicalHardware(QuantumHardwareModel):
     def __init__(self, qubits: Optional[List[LogicalQubit]]=None, couplings: Optional[List[LogicalQubitCoupling]]=None):
         super().__init__()
-        self.qubits = qubits
+        self._qubits = qubits
         self.qubit_direction_couplings = couplings
 
+    @property
+    def qubits(self):
+        return self._qubits
 
-    def get_hw_x_pi_2(self, qubit:LogicalQubit)->LogicalGate:
-        return LogicalGate(qubit=qubit.index, gate=Gate.x)
+    def get_hw_x_pi_2(self, qubit:LogicalQubit, *args)->LogicalGate:
+        return [LogicalGate(qubit=qubit.index, gate=Gate.x)]
 
-    def get_hw_z(self, qubit:LogicalQubit, phase:float)->LogicalGate:
-        return LogicalGate(qubit=qubit.index, gate=Gate.rz, angle=phase)
+    def get_hw_z(self, qubit:LogicalQubit, phase:float, *args)->LogicalGate:
+        return [LogicalGate(qubit=qubit.index, gate=Gate.rz, angle=phase)]
 
-    def get_hw_zx_pi_4(self, qubit: LogicalQubit, target_qubit: LogicalQubit) -> LogicalGate:
-        return LogicalGate(qubit=qubit.index, gate=Gate.zx, target_qubit=target_qubit.index)
+    def get_hw_zx_pi_4(self, qubit: LogicalQubit, target_qubit: LogicalQubit, *args) -> LogicalGate:
+        return [LogicalGate(qubit=qubit.index, gate=Gate.zx, target_qubit=target_qubit.index)]
     
-    def get_gate_X(self, qubit, theta, ):
-        return self.get_gate_U(qubit, theta, -np.pi / 2.0, np.pi / 2.0)
+    def get_gate_X(self, qubit, theta, *args):
+        return [self.get_gate_U(qubit, theta, -np.pi / 2.0, np.pi / 2.0)]
 
     def get_gate_ZX(self, qubit, theta, target_qubit):
         if np.isclose(theta, 0.0):
@@ -87,7 +91,7 @@ class LogicalHardware():
                 self.get_hw_z(target_qubit, np.pi),
             ]
 
-    def get_gate_U(self, qubit, theta, phi, lamb):
+    def get_gate_U(self, qubit, theta, phi, lamb, *args):
         return [
             self.get_hw_z(qubit, lamb + np.pi),
             self.get_hw_x_pi_2(qubit),
@@ -97,6 +101,8 @@ class LogicalHardware():
         ]
     
     # def 
+    def _resolve_qb_pulse_channel(self, target):
+        return target, None
 
     def get_qubit(self, qubit):
         return self.qubits[qubit]
@@ -109,18 +115,23 @@ def get_default_logical_hardware(qubit_count):
     connectivity = [LogicalQubitCoupling((i, (i + 1) % qubit_count)) for i in range(qubit_count)]
     return LogicalHardware(qubits=qubits, couplings=connectivity)
 
-class LogicalBuilder(InstructionBuilder):
+class LogicalBuilder(QuantumInstructionBuilder):
     def __init__(self, hardware_model: QuantumHardwareModel):
         # super().__init__()
         self.model = hardware_model
         self._instructions = []
     
+    def repeat(self, *args, **kwargs):
+        return self
+
     def serialise(self):
-        return ";".join(str(inst.make_dict()) for inst in self._instructions if isinstance(inst, LogicalGate))
+        return ";".join(str(inst.make_dict()) for inst in self._instructions[0].instructions if isinstance(inst, LogicalGate))
 
     def add(self, inst: Union[List[Gate], Gate]):
         if isinstance(inst, List):
-            self._instructions.extend(inst)
+            for ins in inst:
+                self.add(ins)
+            # self._instructions.extend(inst)
         else:
             self._instructions.append(inst)
         return self
@@ -141,6 +152,7 @@ class LogicalBuilder(InstructionBuilder):
         self, target: LogicalQubit, axis: ProcessAxis = None, output_variable: str = None
     ) -> "InstructionBuilder":
         self.add(LogicalGate(qubit=target.index, gate=Gate.meas))
+        return FluidBuilderWrapper(self, None)
 
 
     def cX(self, controllers: Union[LogicalQubit, List[LogicalQubit]], target: LogicalQubit, radii=None):
@@ -157,7 +169,7 @@ class LogicalBuilder(InstructionBuilder):
         instructions.extend(self.model.get_gate_ZX(control, np.pi / 4.0, target))
         instructions.extend(self.model.get_gate_X(control, np.pi))
         instructions.extend(self.model.get_gate_ZX(control, -np.pi / 4.0, target))
-        self.add(instructions)
+        return self.add(instructions)
 
 def get_angle(angle):
     if isinstance(angle, float):
@@ -176,14 +188,28 @@ def convert_to_other(logical_string, hardware:QuantumHardwareModel):
     gates =[ast.literal_eval(string) for string in logical_string.split(";")]
 
     builder = get_builder(hardware)
-
-    for gate in gates:
-        logical_gate = gate['g']
-        qubit = hardware.get_qubit(gate['q'])
-        if logical_gate == Gate.x.name:
-            builder._instructions.append(hardware.get_hw_x_pi_2(qubit=qubit))
-        elif logical_gate == Gate.rz.name:
-            builder._instructions.append(hardware.get_hw_z(qubit=qubit, phase=get_angle(gate['a'])))
-        elif logical_gate == Gate.zx.name:
-            builder._instructions.append(hardware.get_hw_zx_pi_4(qubit=qubit, target_qubit=hardware.get_qubit(gate['t'])))
+    if not isinstance(hardware, QiskitHardwareModel):
+        for gate in gates:
+            logical_gate = gate['g']
+            qubit = hardware.get_qubit(gate['q'])
+            if logical_gate == Gate.x.name:
+                builder._instructions.append(hardware.get_hw_x_pi_2(qubit=qubit))
+            elif logical_gate == Gate.rz.name:
+                builder._instructions.append(hardware.get_hw_z(qubit=qubit, phase=get_angle(gate['a'])))
+            elif logical_gate == Gate.zx.name:
+                builder._instructions.append(hardware.get_hw_zx_pi_4(qubit=qubit, target_qubit=hardware.get_qubit(gate['t'])))
+            # elif logical_gate == Gate.meas.name:
+                # builder._instructions.append()
+    else:
+        for gate in gates:
+            logical_gate = gate['g']
+            qubit = hardware.get_qubit(gate['q'])
+            if logical_gate == Gate.x.name:
+                builder.R(axis=Axis.X, target=qubit, radii=math.pi/2)
+            elif logical_gate == Gate.rz.name:
+                builder.R(axis=Axis.Z, target=qubit, radii=get_angle(gate['a']))
+            elif logical_gate == Gate.zx.name:
+                builder.ECR(control=qubit, target=hardware.get_qubit(gate['t']))
+            # elif logical_gate == Gate.meas.name:
+        builder.circuit.measure_all()
     return builder
