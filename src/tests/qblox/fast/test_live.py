1d3d6af71ebc8a34021a1cb5f16e4c7b29d38cb1
Hamid El Maazouz <hamid.elmaazouz@gmail.com>
2024-07-17 15:39:47 +0100

    execution changes

--- src/tests/qblox/fast/test_live.py
+++ src/tests/qblox/fast/test_live.py
@@ -4,10 +4,11 @@ from qat.purr.backends.qblox.fast.live import FastQbloxLiveEngine
 from src.tests.qblox.builder_nuggets import resonator_spect
 
 
-@pytest.mark.parametrize("model", [None], indirect=True)
+@pytest.mark.parametrize("model", [2], indirect=True)
 class TestFastQbloxLiveEngine:
     def test_execute(self, model):
         builder = resonator_spect(model)
         engine = FastQbloxLiveEngine(model)
+        model.control_hardware.dump_sequence = True
         results = engine.execute(builder.instructions)
         assert results is not None
