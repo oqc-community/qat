16c58ac7d17cec773fde8936064d30fef488cfad
Hamid El Maazouz <hamid.elmaazouz@gmail.com>
2024-09-12 11:30:45 +0100

    Port package timeline and acquisition visualisation and expose debug flags for customers

--- src/qat/purr/backends/qblox/device.py
+++ src/qat/purr/backends/qblox/device.py
@@ -22,6 +22,7 @@ from qat.purr.backends.qblox.config import (
 )
 from qat.purr.backends.qblox.constants import Constants
 from qat.purr.backends.qblox.ir import Sequence
+from qat.purr.backends.qblox.visualisation import plot_acquisitions, plot_packages
 from qat.purr.compiler.devices import (
     ChannelType,
     PhysicalChannel,
@@ -127,6 +128,8 @@ class QbloxControlHardware(ControlHardware):
         self.address = address or os.environ.get("QBLOX_DEV_IP")
         self.dummy_cfg = dummy_cfg
         self.dump_sequence = False
+        self.plot_packages = False
+        self.plot_acquisitions = False
         self._resources: Dict[Module, Dict[PulseChannel, Sequencer]] = {}
 
     def allocate_resources(self, packages: List[QbloxPackage]):
@@ -227,12 +230,6 @@ class QbloxControlHardware(ControlHardware):
                 QrmConfigHelper(config).configure(module, sequencer)
 
         sequence = asdict(package.sequence)
-        if self.dump_sequence:
-            filename = f"schedules/sequence_{module.slot_idx}_{sequencer.seq_idx}_@_{datetime.utcnow().strftime('%m-%d-%Y_%H%M%S')}.json"
-            os.makedirs(os.path.dirname(filename), exist_ok=True)
-            with open(filename, "w") as f:
-                f.write(json.dumps(sequence))
-
         log.debug(f"Uploading sequence to {module}, sequencer {sequencer}")
         sequencer.sequence(sequence)
 
@@ -241,8 +238,15 @@ class QbloxControlHardware(ControlHardware):
     def set_data(self, qblox_packages: List[QbloxPackage]):
         self._resources.clear()
         self.allocate_resources(qblox_packages)
+        if self.plot_packages:
+            plot_packages(qblox_packages)
         for package in qblox_packages:
-            self.install(package)
+            module, sequencer = self.install(package)
+            if self.dump_sequence:
+                filename = f"schedules/target_{package.target.id}_module_{module.slot_idx}_sequencer_{sequencer.seq_idx}_@_{datetime.utcnow().strftime('%m-%d-%Y_%H%M%S')}.json"
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                with open(filename, "w") as f:
+                    f.write(json.dumps(asdict(package.sequence)))
 
     def start_playback(self, repetitions: int, repetition_time: float):
         if not any(self._resources):
@@ -266,6 +270,13 @@ class QbloxControlHardware(ControlHardware):
                             "Two or more pulse channels on the same physical channel"
                         )
                     acquisitions = self._get_acquisitions(module, sequencer)
+
+                    if self.plot_acquisitions:
+                        plot_acquisitions(
+                            acquisitions,
+                            integration_length=sequencer.integration_length_acq(),
+                        )
+
                     for acq_name, acq in acquisitions.items():
                         i = np.array(acq["acquisition"]["bins"]["integration"]["path0"])
                         q = np.array(acq["acquisition"]["bins"]["integration"]["path1"])
