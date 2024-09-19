6297a3ebc40046bb80eda4e79970cf2903035d5b
Daria Van Hende <dvanhende@oxfordquantumcircuits.com>
2024-09-19 15:51:07 +0100

    Add support for env variables as well.

--- src/qat/purr/qatconfig.py
+++ src/qat/purr/qatconfig.py
@@ -1,7 +1,8 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class QatConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="QAT_")
     MAX_REPEATS_LIMIT: int = 100_000
     """Max number of repeats / shots to be performed in a single job."""
 
