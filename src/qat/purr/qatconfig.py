a71ba01131381527c6b80ebe68ad531053f110a0
Daria Van Hende <dvanhende@oxfordquantumcircuits.com>
2024-09-19 15:55:46 +0100

    Add docu.

--- src/qat/purr/qatconfig.py
+++ src/qat/purr/qatconfig.py
@@ -2,6 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class QatConfig(BaseSettings):
+    """
+    Full settings for a single job. All values are defaulted on initialization.
+    """
+
     model_config = SettingsConfigDict(env_prefix="QAT_")
     MAX_REPEATS_LIMIT: int = 100_000
     """Max number of repeats / shots to be performed in a single job."""
