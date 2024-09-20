878eddb163ca0653ee9838a68d9e570e675048db
Daria Van Hende <dvanhende@oxfordquantumcircuits.com>
2024-09-20 12:03:41 +0100

    Error throw on invalid assignment shot limit + tests.

--- tests/qat/test_qatconfig.py
+++ tests/qat/test_qatconfig.py
@@ -10,3 +10,9 @@ def test_changed_max_repeats_limit(repeat_limits):
     # Default value is 100_000. Test manual change of repeats limit.
     qatconfig.MAX_REPEATS_LIMIT = repeat_limits
     assert qatconfig.MAX_REPEATS_LIMIT == repeat_limits
+
+
+@pytest.mark.parametrize("invalid_argument", ["5", "invalid", {"key": 5}, 5.5])
+def test_qatconfig_invalid_assignment(invalid_argument):
+    with pytest.raises(TypeError):
+        qatconfig.MAX_REPEATS_LIMIT = invalid_argument
