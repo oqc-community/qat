4aa6efc934eebdc1347f470683d316b8d5069aa4
Daria Van Hende <dvanhende@oxfordquantumcircuits.com>
2024-09-20 14:22:02 +0100

    Remove fixture.

--- tests/qat/test_qatconfig.py
+++ tests/qat/test_qatconfig.py
@@ -3,24 +3,20 @@
 import pytest
 from pydantic import ValidationError
 
+from qat import qatconfig
 
-# from qat import qatconfig
-@pytest.fixture
-def qatconfig():
-    """We import qatconfig as a fixture so we can test mocking the environment."""
-    from qat import qatconfig
-
-    yield qatconfig
+MAX_REPEATS_LIMIT = 100_000
+qatconfig.MAX_REPEATS_LIMIT = MAX_REPEATS_LIMIT
 
 
 @pytest.mark.parametrize("invalid_argument", ["invalid", {"key": 5}, 5.5])
-def test_qatconfig_invalid_assignment(qatconfig, invalid_argument):
+def test_qatconfig_invalid_assignment(invalid_argument):
     with pytest.raises(ValidationError):
         qatconfig.MAX_REPEATS_LIMIT = invalid_argument
 
 
 @pytest.mark.parametrize("repeats_limit", [10_000, 50_000, 16_874, 100_000])
-def test_change_max_repeats_limit(qatconfig, repeats_limit):
+def test_change_max_repeats_limit(repeats_limit):
     qatconfig.__init__()  # Reload settings.
     # Test direct change of repeats limit.
     qatconfig.MAX_REPEATS_LIMIT = repeats_limit
@@ -28,7 +24,7 @@ def test_change_max_repeats_limit(qatconfig, repeats_limit):
 
 
 @pytest.mark.parametrize("repeats_limit", [10_000, 50_000, 16_874, 100_000])
-def test_change_env_max_repeats_limit(qatconfig, monkeypatch, repeats_limit):
+def test_change_env_max_repeats_limit(monkeypatch, repeats_limit):
     monkeypatch.setenv("QAT_MAX_REPEATS_LIMIT", str(repeats_limit))
     # Default value cannot change if env variable changes after instantiation.
-    assert qatconfig.MAX_REPEATS_LIMIT == 100_000
+    assert qatconfig.MAX_REPEATS_LIMIT == MAX_REPEATS_LIMIT
