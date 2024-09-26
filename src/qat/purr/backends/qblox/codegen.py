1ccc209cffd84771224285d2c715291e6915b835
Hamid El Maazouz <helmaazouz@oxfordquantumcircuits.com>
2024-09-26 16:52:32 +0100

    Rely on time padding based SYNC only

--- src/qat/purr/backends/qblox/codegen.py
+++ src/qat/purr/backends/qblox/codegen.py
@@ -262,6 +262,8 @@ class QbloxContext:
         self.sequence_builder.nop()
         self.sequence_builder.label(self._repeat_label)
         self.sequence_builder.reset_ph()
+        self.sequence_builder.upd_param(Constants.GRID_TIME)
+        self.sequence_builder.wait_sync(Constants.GRID_TIME)
 
         return self
 
@@ -540,7 +542,9 @@ class QbloxContext:
             cxt = contexts[target]
             delay_time = max_duration - cxt.duration
             cxt.delay(Delay(target, delay_time))
-            cxt.sequence_builder.wait_sync(Constants.GRID_TIME)
+            # TODO - For now, enable only logical time padding
+            # TODO - Enable when finer grained SYNC groups are supported
+            # cxt.sequence_builder.wait_sync(Constants.GRID_TIME)
 
     @staticmethod
     def reset_phase(inst: PhaseReset, contexts: Dict):
