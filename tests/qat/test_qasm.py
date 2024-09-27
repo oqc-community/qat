1451dd76eb8068e90d8d83303c9b037cae8456f2
lcauser-oqc <lcauser@oxfordquantumcircuits.com>
2024-09-27 11:55:38 +0100

    finer detail tests for overridden gates

--- tests/qat/test_qasm.py
+++ tests/qat/test_qasm.py
@@ -229,37 +229,81 @@ class TestQASM3:
         )
 
     def test_ecr_override(self):
+        """
+        Tests overriding an ECR using openpulse: checks the overridden ECR
+        yields the correct pulses, and that the unchanged ECRs are the same
+        as those created by the circuit builder.
+        """
         hw = get_default_echo_hardware(4)
         parser = Qasm3Parser()
         result = parser.parse(get_builder(hw), get_qasm3("ecr_override_test.qasm"))
-        # tests that there are 2 extra_soft_square pulses from the custom def
+        qasm_inst = result.instructions
+        qasm_inst_names = [str(inst) for inst in qasm_inst]
+
+        # test the extra_soft_square pulses are as expected
+        ess_pulses = [
+            inst
+            for inst in qasm_inst
+            if hasattr(inst, "shape") and (inst.shape is PulseShapeType.EXTRA_SOFT_SQUARE)
+        ]
+        assert len(ess_pulses) == 2
+        assert all([len(inst.quantum_targets) == 1 for inst in ess_pulses])
+        assert ess_pulses[0].quantum_targets[0].partial_id() == "q1_frame"
+        assert ess_pulses[1].quantum_targets[0].partial_id() == "q2_frame"
+
+        # test the ecrs on (0, 1) and (2, 3) are unchanged by the override
+        circuit = hw.create_builder()
+        circuit.ECR(hw.get_qubit(0), hw.get_qubit(1))
+        circ_inst = circuit.instructions
+        circ_inst_names = [str(inst) for inst in circ_inst]
+        assert qasm_inst_names[0 : len(circ_inst_names)] == circ_inst_names
+
+        circuit = hw.create_builder()
+        circuit.ECR(hw.get_qubit(2), hw.get_qubit(3))
+        circ_inst = circuit.instructions
+        circ_inst_names = [str(inst) for inst in circ_inst]
         assert (
-            len(
-                [
-                    inst
-                    for inst in result.instructions
-                    if hasattr(inst, "shape")
-                    and (inst.shape is PulseShapeType.EXTRA_SOFT_SQUARE)
-                ]
-            )
-            == 2
+            qasm_inst_names[len(qasm_inst_names) - len(circ_inst_names) :]
+            == circ_inst_names
         )
 
     def test_cnot_override(self):
+        """
+        Tests overriding a CNOT using openpulse: checks the overridden CNOT
+        yields the correct pulses, and that the unchanged CNOTS are the same
+        as those created by the circuit builder.
+        """
         hw = get_default_echo_hardware(4)
         parser = Qasm3Parser()
         result = parser.parse(get_builder(hw), get_qasm3("cnot_override_test.qasm"))
-        # tests that there are 2 extra_soft_square pulses from the custom def
+        qasm_inst = result.instructions
+        qasm_inst_names = [str(inst) for inst in qasm_inst]
+
+        # test the extra_soft_square pulses are as expected
+        ess_pulses = [
+            inst
+            for inst in qasm_inst
+            if hasattr(inst, "shape") and (inst.shape is PulseShapeType.EXTRA_SOFT_SQUARE)
+        ]
+        assert len(ess_pulses) == 2
+        assert all([len(inst.quantum_targets) == 1 for inst in ess_pulses])
+        assert ess_pulses[0].quantum_targets[0].partial_id() == "q1_frame"
+        assert ess_pulses[1].quantum_targets[0].partial_id() == "q2_frame"
+
+        # test the cnots on (0, 1) and (2, 3) are unchanged by the override
+        circuit = hw.create_builder()
+        circuit.cnot(hw.get_qubit(0), hw.get_qubit(1))
+        circ_inst = circuit.instructions
+        circ_inst_names = [str(inst) for inst in circ_inst]
+        assert qasm_inst_names[0 : len(circ_inst_names)] == circ_inst_names
+
+        circuit = hw.create_builder()
+        circuit.cnot(hw.get_qubit(2), hw.get_qubit(3))
+        circ_inst = circuit.instructions
+        circ_inst_names = [str(inst) for inst in circ_inst]
         assert (
-            len(
-                [
-                    inst
-                    for inst in result.instructions
-                    if hasattr(inst, "shape")
-                    and (inst.shape is PulseShapeType.EXTRA_SOFT_SQUARE)
-                ]
-            )
-            == 2
+            qasm_inst_names[len(qasm_inst_names) - len(circ_inst_names) :]
+            == circ_inst_names
         )
 
     @pytest.mark.parametrize(
@@ -275,7 +319,7 @@ class TestQASM3:
     def test_u_gate(self, params):
         """
         Tests the validty of the U gate with OpenPulse by checking that the
-        parsed circuit matches the same circuit created with teh cirucit builder.
+        parsed circuit matches the same circuit created with the circuit builder.
         """
         hw = get_default_echo_hardware(2)
 
@@ -305,7 +349,7 @@ class TestQASM3:
         # validate that the circuits match
         assert len(qasm_inst) == len(circ_inst)
         for i in range(len(qasm_inst)):
-            assert qasm_inst[i].__dict__ == circ_inst[i].__dict__
+            assert str(qasm_inst[i]) == str(circ_inst[i])
 
     def test_invalid_frames(self):
         hw = get_default_echo_hardware()
