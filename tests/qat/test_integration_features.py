64dacb2074f61e2e3a1de1f58f0d24d9c7a76028
Benjamin Sach <bsach@oxfordquantumcircuits.com>
2024-10-14 13:59:50 +0100

    frozen dataclasses and basic tests

--- tests/qat/test_integration_features.py
+++ tests/qat/test_integration_features.py
@@ -1,6 +1,28 @@
+import pytest
+
+from qat.purr.backends.echo import get_default_echo_hardware
+from qat.purr.backends.qiskit_simulator import get_default_qiskit_hardware
+from qat.purr.backends.realtime_chip_simulator import get_default_RTCS_hardware
 from qat.purr.integrations.features import OpenPulseFeatures
 
 
-def test_openpulsefeatures():
+@pytest.mark.parametrize(
+    "get_hardware",
+    [
+        get_default_echo_hardware,
+        get_default_qiskit_hardware,
+        get_default_RTCS_hardware,
+    ],
+)
+def test_openpulsefeatures(get_hardware):
     opf = OpenPulseFeatures()
-    assert len(opf.ports) == 0
+    opf.for_hardware(get_hardware())
+    blob = opf.to_json_dict()
+    assert ["open_pulse"] == list(blob.keys())
+    assert list(blob["open_pulse"].keys()) == [
+        "version",
+        "ports",
+        "frames",
+        "waveforms",
+        "constraints",
+    ]
