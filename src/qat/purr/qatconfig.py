23f2302bb8792cc95ed9261b93325abf8c038b1a
lcauser-oqc <lcauser@oxfordquantumcircuits.com>
2024-10-19 17:22:50 +0100

    enable qiskit aersimulator options via qatconfig

--- src/qat/purr/qatconfig.py
+++ src/qat/purr/qatconfig.py
@@ -1,11 +1,45 @@
-from typing import Optional
+from typing import Literal, Optional
 
-from pydantic import Field, field_validator
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from qiskit_aer import AerSimulator
 
 from qat.purr.utils.logger import get_default_logger
 
 log = get_default_logger()
+
+
+class QiskitSimulationConfig(BaseModel):
+    """
+    The default settings for the Qiskit Simulator, including overridden MPS settings.
+    """
+
+    model_config = ConfigDict(validate_assignment=True)
+    allowed_methods: type = Literal[AerSimulator().available_methods()]
+    METHOD: allowed_methods = "automatic"
+    """The simulation method to use."""
+    FALLBACK_SEQUENCE: list[allowed_methods] = ["automatic", "matrix_product_state"]
+    """If the simulation fails, specify a fallback sequence of methods to call."""
+    OPTIONS: dict = {
+        "matrix_product_state_max_bond_dimension": 128,
+        "matrix_product_state_truncation_threshold": 1e-12,
+    }
+    """
+    Specify the options for a chosen AerSimulator backend. See
+    https://docs.quantum.ibm.com/api/qiskit/0.37/qiskit.providers.aer.AerSimulator
+    for options you can provide.
+    """
+    ENABLE_METADATA: bool = False
+    """Returns the AerSimulator metadata if enabled."""
+
+
+class QatSimulationConfig(BaseModel):
+    """
+    The default settings for QATs simulation backends.
+    """
+
+    model_config = ConfigDict(validate_assignment=True)
+    QISKIT: QiskitSimulationConfig = QiskitSimulationConfig()
 
 
 class QatConfig(BaseSettings):
@@ -32,12 +66,17 @@ class QatConfig(BaseSettings):
     Input should be a valid integer, got a number with a fractional part
     """
 
-    model_config = SettingsConfigDict(env_prefix="QAT_", validate_assignment=True)
+    model_config = SettingsConfigDict(
+        env_prefix="QAT_", env_nested_delimiter="_", validate_assignment=True
+    )
     MAX_REPEATS_LIMIT: Optional[int] = Field(gt=0, default=100_000)
     """Max number of repeats / shots to be performed in a single job."""
     DISABLE_PULSE_DURATION_LIMITS: bool = False
     """Flag to disable the lower and upper pulse duration limits. 
     Only needs to be set to True for calibration purposes."""
+
+    SIMULATION: QatSimulationConfig = QatSimulationConfig()
+    """Options for QATs simulation backends."""
 
     @field_validator("DISABLE_PULSE_DURATION_LIMITS")
     def check_disable_pulse_duration_limits(cls, DISABLE_PULSE_DURATION_LIMITS):
