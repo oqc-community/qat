0ceb7845a3460b80576f67cade2d22bbcf6bdce7
Daria Van Hende <dvanhende@oxfordquantumcircuits.com>
2024-11-05 15:18:31 +0000

    Add documentation and relevant device variables.

--- tests/qat/model/test_pydantic_hw_serialisation.py
+++ tests/qat/model/test_pydantic_hw_serialisation.py
@@ -14,6 +14,7 @@ from qat.model.device import (
     Resonator,
 )
 from qat.model.hardware_model import QuantumHardwareModel
+from qat.purr.compiler.devices import ChannelType
 
 
 def make_Hardware(count=10, connections=3, seed=42):
@@ -26,13 +27,34 @@ def make_Hardware(count=10, connections=3, seed=42):
             if pulse_ch.physical_channel == physical_channel
         ]
     )
-    physical_basebands = [PhysicalBaseband() for _ in range(count)]
+    physical_basebands = [
+        PhysicalBaseband(
+            frequency=np.random.uniform(1e05, 1e07),
+            if_frequency=np.random.uniform(1e05, 1e07),
+        )
+        for _ in range(count)
+    ]
     physical_channels = [
-        PhysicalChannel(baseband=list(pick(physical_basebands, 1).values())[0])
-        for _ in range(count // 5)
+        PhysicalChannel(
+            baseband=list(pick(physical_basebands, 1).values())[0],
+            sample_time=np.random.uniform(1e-10, 1e-08),
+            block_size=np.random.randint(1, 10),
+            phase_iq_offset=np.random.uniform(0.0, 1.0),
+            bias=np.random.uniform(-1.0, 1.0),
+            acquire_allowed=rng.choice([True, False], size=1)[0],
+        )
+        for _ in range(count)
     ]
     pulse_channels = [
-        PulseChannel(physical_channel=physical_channel, auxiliary_qubits=[])
+        PulseChannel(
+            physical_channel=physical_channel,
+            frequency=np.random.uniform(1e08, 1e10),
+            bias=np.random.uniform(-1, 1) + 1.0j * np.random.uniform(-1, 1),
+            scale=np.random.uniform(-10, 10) + 1.0j * np.random.uniform(-10, 10),
+            fixed_if=rng.choice([True, False], size=1)[0],
+            channel_type=rng.choice(list(ChannelType), size=1)[0],
+            auxiliary_qubits=[],
+        )
         for physical_channel in physical_channels
     ]
     resonators = [
@@ -41,16 +63,20 @@ def make_Hardware(count=10, connections=3, seed=42):
             pulse_channels=pick_pulse_channels(
                 pulse_channels=pulse_channels, physical_channel=physical_channels[0]
             ),
+            default_channel_type=rng.choice(list(ChannelType), size=1)[0],
         )
         for physical_channel in list(pick(physical_channels, count // 2).values())
     ]
     qubits = [
         Qubit(
+            index=np.random.randint(0, count),
+            drive_amp=np.random.uniform(-10, 10),
             physical_channel=physical_channel,
             pulse_channels=pick_pulse_channels(
                 pulse_channels=pulse_channels, physical_channel=physical_channels[0]
             ),
             measure_device=list(pick(resonators, 1).values())[0],
+            default_channel_type=rng.choice(list(ChannelType), size=1)[0],
         )
         for physical_channel in list(pick(physical_channels, count // 2).values())
     ]
@@ -100,5 +126,5 @@ class Test_HW_Serialise:
 
         assert O2._deepequals(O1)
         pc = list(O2.pulse_channels.values())[0]
-        pc.some_val = -1
+        pc.frequency = -1
         assert not O2._deepequals(O1)
