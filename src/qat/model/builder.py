5681e002e957ad1f977bd273534677a934b232a4
Daria Van Hende <dvanhende@oxfordquantumcircuits.com>
2024-11-06 15:55:14 +0000

    Add support for other hw components.

--- src/qat/model/builder.py
+++ src/qat/model/builder.py
@@ -1,15 +1,25 @@
 from typing import Annotated, get_args, get_origin
 
 from qat.model.component import Component
+from qat.model.device import (
+    PhysicalBaseband,
+    PhysicalChannel,
+    PulseChannel,
+    Qubit,
+    Resonator,
+)
 from qat.model.hardware_model import QuantumHardwareModel
+from qat.purr.compiler.devices import ChannelType
+
+from qat.model.serialisation import ComponentDict  # isort: skip
 
 
 class QuantumHardwareModelBuilder:
     def __init__(self):
         self.model = QuantumHardwareModel()
 
-    def add_component(self, component_type: Component, **kwargs):
-        component = component_type(**kwargs)
+    def _add_component(self, component: Component):
+        component_type = type(component)
 
         for field_name, field_type in self.model.__annotations__.items():
             if (
@@ -23,6 +33,64 @@ class QuantumHardwareModelBuilder:
                 return
 
         raise Exception(f"Unknown hardware component type {component_type}.")
+
+    def add_physical_baseband(self, frequency, **kwargs):
+        self._add_component(PhysicalBaseband(frequency=frequency, **kwargs))
+
+    def add_physical_channel(self, baseband, sample_time, **kwargs):
+        self._add_component(
+            PhysicalChannel(baseband=baseband, sample_time=sample_time, **kwargs)
+        )
+
+    def add_pulse_channel(self, frequency, physical_channel, **kwargs):
+        self._add_component(
+            PulseChannel(frequency=frequency, physical_channel=physical_channel, **kwargs)
+        )
+
+    def add_qubit(
+        self, index, frequency, physical_channel, resonator, pulse_channels=None, **kwargs
+    ):
+        if pulse_channels is None:
+            pc_drive = PulseChannel(
+                frequency=frequency,
+                physical_channel=physical_channel,
+                channel_type=ChannelType.drive,
+            )
+            pulse_channels = {pc_drive.uuid: pc_drive}
+
+        self._add_component(
+            Qubit(
+                index=index,
+                pulse_channels=pulse_channels,
+                physical_channel=physical_channel,
+                resonator=resonator,
+                **kwargs,
+            )
+        )
+
+    def add_resonator(self, frequency, physical_channel, pulse_channels=None, **kwargs):
+        if pulse_channels is None:
+            pc_measure = PulseChannel(
+                id=ChannelType.measure.name,
+                channel_type=ChannelType.measure,
+                frequency=frequency,
+                physical_channel=physical_channel,
+            )
+            pc_acquire = PulseChannel(
+                id=ChannelType.acquire.name,
+                channel_type=ChannelType.acquire,
+                frequency=frequency,
+                physical_channel=physical_channel,
+            )
+            pulse_channels = {
+                pc_measure.uuid: pc_measure,
+                pc_acquire.uuid: pc_acquire,
+            }
+
+        resonator = Resonator(
+            pulse_channels=pulse_channels, physical_channel=physical_channel, **kwargs
+        )
+        self._add_component(resonator)
 
 
 def get_typevar_from_annotated(type_name: str):
