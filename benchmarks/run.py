d25d12db8d4c809e10d56d9f659a802b060591f4
lcauser-oqc <lcauser@oxfordquantumcircuits.com>
2024-11-11 17:59:02 +0000

    brought over mock models

--- benchmarks/run.py
+++ benchmarks/run.py
@@ -8,8 +8,6 @@ from qat.purr.backends.realtime_chip_simulator import get_default_RTCS_hardware
 from qat.purr.compiler.emitter import InstructionEmitter
 from qat.purr.compiler.frontends import QASMFrontend
 
-from tests.qat.utils.models import get_jagged_echo_hardware
-
 experiments = {}
 
 # Two qubit benchmarks
@@ -26,7 +24,6 @@ for circ in circuits_two_qubits:
 # Ten qubit benchmarks
 hardware_ten_qubits = {
     "echo": get_default_echo_hardware(10),
-    "jagged": get_jagged_echo_hardware(10),
     "mock_live": get_mock_live_hardware(10),
 }
 circuits_ten_qubits = ["10qb_ghz", "10qb_random_cnot"]
