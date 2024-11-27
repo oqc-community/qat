adbad04d2fa8f377a5e5037fd22dfc92dbf6f678
Kajsa <58070002+keriksson-rosenqvist@users.noreply.github.com>
2024-11-27 14:40:22 +0000

    Instruction list optimisation and result processing passes. (#271)
    * Optimise instructions with passes.
    
    * Result processing passes.

---
+++ src/qat/runtime/analysis_passes.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from compiler_config.config import CompilerConfig
+
+from qat.ir.pass_base import AnalysisPass
+from qat.ir.result_base import ResultInfoMixin, ResultManager
+from qat.purr.backends.calibrations.remote import find_calibration
+from qat.purr.compiler.builders import InstructionBuilder
+from qat.purr.compiler.runtime import CalibrationWithArgs
+
+
+@dataclass
+class CalibrationAnalysisResult(ResultInfoMixin):
+    calibration_executables: List[CalibrationWithArgs] = field(default_factory=list)
+
+
+class CalibrationAnalysis(AnalysisPass):
+    def __init__(self, compiler_config: CompilerConfig):
+        self.compiler_config = compiler_config
+
+    def run(self, builder: InstructionBuilder, res_mgr: ResultManager, *args, **kwargs):
+        cal_blocks = [
+            find_calibration(arg) for arg in self.compiler_config.active_calibrations
+        ]
+        res_mgr.add(CalibrationAnalysisResult(cal_blocks))
