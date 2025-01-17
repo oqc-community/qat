aef91232fce5919bd777dfa677d7be01b8b1b911
lcauser-oqc <lcauser@oxfordquantumcircuits.com>
2025-01-17 15:09:19 +0000

    Acquire and post-processing documentation (#51)
    * documented enums
    
    * minor style change
    
    * typo
    
    * adjusted description
    
    * COMPILER-235
    
    * COMPILER-235 minor change to ProcessAxis
    
    * typo
    
    * typos
    
    GitOrigin-RevId: fe175b4e72118d23ef2fdc1ffe5f8ed04c81a784

--- src/qat/purr/compiler/instructions.py
+++ src/qat/purr/compiler/instructions.py
@@ -23,6 +23,19 @@ def _stringify_qubits(qubits):
 
 
 class PostProcessType(Enum):
+    """
+    `PostProcessType` is used in a `PostProcessing` instruction to determine how readouts
+    should be processed. It describes a type of classical post-processing:
+
+        - `DOWN_CONVERT`: Down-converts the readout measurement, should only be used along the
+        axis `ProcessAxis.TIME`.
+        - `MEAN`: Takes an average over the given axis.
+        - `LINEAR_MAP_COMPLEX_TO_REAL`: Maps the (complex) measured value onto a (real)
+        z-projection using a supplied linear mapping.
+        - `DISCRIMINATE`: Converts a z-projection to a classical bit by comparison with a
+        supplied discrimination threshold.
+    """
+
     DOWN_CONVERT = "down_convert"
     MEAN = "mean"
     LINEAR_MAP_COMPLEX_TO_REAL = "linear_map_real"
@@ -34,6 +47,16 @@ class PostProcessType(Enum):
 
 
 class ProcessAxis(Enum):
+    """
+    `ProcessAxis` is used during classical post-processing of readouts. It specifies the axis
+    which the post-processing should occur on. Often used in conjunction with the
+    `AcquireMode` to determine the correct method for post-processing.
+
+        - `TIME`: Instructs the post-processing to be performed over the time-series data
+        returned from a readout.
+        - `SEQUENCE`: Instructs the post-processing to be performed over the shots.
+    """
+
     TIME = "time"
     SEQUENCE = "sequence"
 
@@ -165,6 +188,19 @@ class Id(QuantumInstruction):
 
 
 class AcquireMode(Enum):
+    """
+    The `AcquireMode` is used to specify the type of acquisition at the level of the control
+    hardware.
+
+    Note that different backends will only allow selected acquisition modes.
+
+        - `RAW`: Returns the time-series acquisition data for each shot
+        - `SCOPE`: Returns the time-series acquisition data where each point is averaged over
+        the number of shots
+        - `INTEGRATOR`: Down-conversion and integration of the acquisition data has occurred on
+        the hardware so that a single data point is returned for each shot
+    """
+
     RAW = "raw"
     SCOPE = "scope"
     INTEGRATOR = "integrator"
