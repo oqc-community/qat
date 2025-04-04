bf08294b7f7c61ec352e2efb338a8cd6463ab7d9
Kajsa <58070002+keriksson-rosenqvist-oqc@users.noreply.github.com>
2025-04-04 11:52:20 +0100

    COMPILER 387: Validate Lowered Pulse Amplitudes. (#149)
    GitOrigin-RevId: 6573bd387fa90f1476c696d124c157405a491fe2

--- src/qat/middleend/passes/legacy/validation.py
+++ src/qat/middleend/passes/legacy/validation.py
@@ -1,13 +1,104 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2025 Oxford Quantum Circuits Ltd
-
+import numpy as np
 from compiler_config.config import CompilerConfig, QuantumResultsFormat, ResultsFormatting
 
 from qat.core.pass_base import ValidationPass
 from qat.purr.backends.qiskit_simulator import QiskitBuilder
+from qat.purr.compiler.builders import InstructionBuilder
+from qat.purr.compiler.instructions import CustomPulse, Pulse, PulseShapeType, Synchronize
 from qat.purr.utils.logger import get_default_logger
 
 log = get_default_logger()
+
+
+class PhysicalChannelAmplitudeValidation(ValidationPass):
+    """
+    Validates that the total amplitude of pulses on a physical channel does not exceed
+    allowed levels.
+
+    Data is stored in a nested dict structure
+
+    .. code-block:: python
+
+        { 'CH1': {
+                      CH1.Q0.drive: 0.2+0.5j,
+                      CH1.Q0.Q1.cross_resonsance: 0.4+0.0j,
+                  },
+          'CH2': { CH2.R0.measure: 0.9-0.6j },
+        }
+
+    This pass requires all :class:`Pulse` s to have been lowered to either :class:`PulseShapeType.SQUARE`
+    or a :class:`CustomPulse` with the evaluated pulse `samples`. Any non lowered pulses with
+    other types may cause the pipeline to fail.
+    """
+
+    def run(self, ir: InstructionBuilder, *args, **kwargs):
+        """
+        :param ir: The intermediate representation (IR) :class:`InstructionBuilder`.
+        :raises ValueError: for :class:`Pulse` of non :class:`PulseShapeType.SQUARE` type.
+        """
+        # TODO: Evaluate if this would be better if operating on `PartitionedIR` class
+        # instead of `InstructionBuilder`
+
+        phys_chs_amps: dict[str, dict[str, complex]] = {}
+        for pulse_channel in ir.model.pulse_channels.values():
+            pulse_ch_amps = phys_chs_amps.setdefault(pulse_channel.physical_channel_id, {})
+            pulse_ch_amps[pulse_channel] = 0.0
+
+        for instruction in ir.instructions:
+            if isinstance(instruction, Pulse | CustomPulse):
+                pulse_ch_amps = phys_chs_amps[
+                    phys_chan_id := instruction.channel.physical_channel_id
+                ]
+                pulse_chan = instruction.channel
+                if isinstance(instruction, CustomPulse):
+                    pulse_shape = np.array(instruction.samples)
+
+                    # TODO: This may be part of the lowering or needed here.
+                    # Needs to be determined and potentially adjusted as part of:
+                    # COMPILER-???
+                    # if not instruction.ignore_channel_scale:
+                    #    pulse_shape *= instruction.channel.scale
+
+                    pulse_ch_amps[pulse_chan] = complex(
+                        max(np.abs(pulse_shape.real)) + 1j * max(np.abs(pulse_shape.imag))
+                    )
+                else:
+                    if not instruction.shape == PulseShapeType.SQUARE:
+                        raise ValueError(
+                            f"Can not validate amplitude of un-lowered {instruction.shape} Pulse."
+                        )
+                    pulse_amp = complex(
+                        instruction.scale_factor
+                        * instruction.amp
+                        * np.exp(1.0j * instruction.phase)
+                    )
+                    if not instruction.ignore_channel_scale:
+                        pulse_amp *= instruction.channel.scale
+
+                    pulse_ch_amps[pulse_chan] = pulse_amp
+
+                total_amp = sum(pulse_ch_amps.values())
+
+                if np.abs(np.real(total_amp)) > 1 or np.abs(np.imag(total_amp)) > 1:
+                    raise ValueError(
+                        f"Overflow error detect on {phys_chan_id}!"
+                        " The sum of logical channels onto a physical channel is too high!"
+                    )
+            elif isinstance(instruction, Synchronize):
+                handled_phys_chan_ids = []
+                for target in instruction.quantum_targets:
+                    phys_chan_id = target.physical_channel_id
+                    if phys_chan_id in handled_phys_chan_ids:
+                        continue
+
+                    tmp = phys_chs_amps[phys_chan_id]
+                    if all([chan in instruction.quantum_targets for chan in tmp.keys()]):
+                        phys_chs_amps[phys_chan_id] = dict.fromkeys(tmp.keys(), 0.0)
+                    handled_phys_chan_ids.append(phys_chan_id)
+
+        return ir
 
 
 class QiskitResultsFormatValidation(ValidationPass):
--- tests/unit/middleend/passes/test_validation_passes.py
+++ tests/unit/middleend/passes/test_validation_passes.py
@@ -2,12 +2,16 @@
 # Copyright (c) 2025 Oxford Quantum Circuits Ltd
 from copy import deepcopy
 
+import numpy as np
 import pytest
 
 from qat.ir.instruction_builder import (
     QuantumInstructionBuilder as PydQuantumInstructionBuilder,
 )
+from qat.middleend.passes.legacy.validation import PhysicalChannelAmplitudeValidation
 from qat.middleend.passes.validation import PydNoMidCircuitMeasurementValidation
+from qat.model.loaders.legacy import EchoModelLoader
+from qat.purr.compiler.instructions import CustomPulse, Pulse, PulseShapeType
 from qat.utils.hardware_model import generate_hw_model
 
 
@@ -37,3 +41,261 @@ class TestNoMidCircuitMeasurementValidation:
         p = PydNoMidCircuitMeasurementValidation(self.hw)
         with pytest.raises(ValueError):
             p.run(builder)
+
+
+class TestPhysicalChannelAmplitudeValidation:
+    model = EchoModelLoader(qubit_count=2).load()
+
+    def test_valid_custom_pulse(self):
+        # Create custom sine waveform pulse
+        t = np.linspace(0.0, 10 * np.pi, num=200)
+        samples = (np.sin(t) + 1j * np.cos(t)).tolist()
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(CustomPulse(qubit.get_drive_channel(), samples))
+        ir = PhysicalChannelAmplitudeValidation().run(builder)
+
+        assert builder == ir
+
+    @pytest.mark.parametrize("real_coeff, imag_coeff", [(1, 2), (2, 1), (2, 2)])
+    def test_invalid_custom_pulse(self, real_coeff, imag_coeff):
+        # Create custom sine waveform pulse
+        t = np.linspace(0.0, 10 * np.pi, num=200)
+        samples = (real_coeff * np.sin(t) + imag_coeff * 1j * np.cos(t)).tolist()
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(CustomPulse(qubit.get_drive_channel(), samples))
+        with pytest.raises(ValueError):
+            PhysicalChannelAmplitudeValidation().run(builder)
+
+    def test_exceeding_custom_pulse_sum(self):
+        # Create custom sine waveform pulse
+        t = np.linspace(0.0, 10 * np.pi, num=200)
+        samples = (np.sin(t) + 1j * np.cos(t)).tolist()
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(CustomPulse(qubit.get_drive_channel(), samples))
+        builder.add(
+            CustomPulse(qubit.get_cross_resonance_channel(self.model.get_qubit(1)), samples)
+        )
+        with pytest.raises(ValueError):
+            PhysicalChannelAmplitudeValidation().run(builder)
+
+    def test_valid_square_pulse(self):
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(
+            Pulse(qubit.get_measure_channel(), PulseShapeType.SQUARE, width=1e-06, amp=0.9)
+        )
+        ir = PhysicalChannelAmplitudeValidation().run(builder)
+
+        assert builder == ir
+
+    @pytest.mark.parametrize(
+        "shape", [s for s in list(PulseShapeType) if s != PulseShapeType.SQUARE]
+    )
+    def test_invalid_pulse_shape(self, shape):
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(Pulse(qubit.get_measure_channel(), shape=shape, width=1e-06, amp=0.9))
+        with pytest.raises(ValueError, match=f"un-lowered {shape}"):
+            PhysicalChannelAmplitudeValidation().run(builder)
+
+    @pytest.mark.parametrize("amp, scale", [(1.2, 1), (1, 5)])
+    @pytest.mark.parametrize("phase", [0, np.pi / 2, np.pi, 3 * np.pi / 2])
+    def test_invalid_square_pulse(self, amp, scale, phase):
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(
+            Pulse(
+                qubit.get_measure_channel(),
+                PulseShapeType.SQUARE,
+                width=1e-06,
+                amp=amp,
+                scale_factor=scale,
+                phase=phase,
+            )
+        )
+        with pytest.raises(ValueError):
+            PhysicalChannelAmplitudeValidation().run(builder)
+
+    def test_exceeding_square_pulse_sum(self):
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(
+            [
+                Pulse(
+                    qubit.get_drive_channel(),
+                    PulseShapeType.SQUARE,
+                    width=1e-06,
+                    amp=0.9,
+                    ignore_channel_scale=True,
+                ),
+                Pulse(
+                    qubit.get_cross_resonance_channel(self.model.get_qubit(1)),
+                    PulseShapeType.SQUARE,
+                    width=1e-06,
+                    amp=0.9,
+                    ignore_channel_scale=True,
+                ),
+            ]
+        )
+        with pytest.raises(ValueError):
+            PhysicalChannelAmplitudeValidation().run(builder)
+
+    def test_exceeding_mixed_pulse_sum(self):
+        # Create custom sine waveform pulse
+        t = np.linspace(0.0, 10 * np.pi, num=200)
+        samples = (np.sin(t) + 1j * np.cos(t)).tolist()
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(
+            [
+                Pulse(
+                    qubit.get_drive_channel(),
+                    PulseShapeType.SQUARE,
+                    width=1e-06,
+                    amp=0.9,
+                    ignore_channel_scale=True,
+                ),
+                CustomPulse(
+                    qubit.get_cross_resonance_channel(self.model.get_qubit(1)),
+                    samples=samples,
+                ),
+            ]
+        )
+        with pytest.raises(ValueError):
+            PhysicalChannelAmplitudeValidation().run(builder)
+
+    def test_same_channel_not_summed(self):
+        # Create custom sine waveform pulse
+        t = np.linspace(0.0, 10 * np.pi, num=200)
+        samples = (np.sin(t) + 1j * np.cos(t)).tolist()
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(
+            Pulse(
+                qubit.get_drive_channel(),
+                PulseShapeType.SQUARE,
+                width=1e-06,
+                amp=0.9,
+                ignore_channel_scale=True,
+            )
+        )
+        builder.add(
+            CustomPulse(
+                qubit.get_drive_channel(),
+                samples=samples,
+            )
+        )
+        ir = PhysicalChannelAmplitudeValidation().run(builder)
+
+        assert ir == builder
+
+    def test_reset_by_sync(self):
+        # Create custom sine waveform pulse
+        t = np.linspace(0.0, 10 * np.pi, num=200)
+        samples = (np.sin(t) + 1j * np.cos(t)).tolist()
+        qubit = self.model.get_qubit(0)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(
+            Pulse(
+                qubit.get_drive_channel(),
+                PulseShapeType.SQUARE,
+                width=1e-06,
+                amp=0.9,
+                ignore_channel_scale=True,
+            )
+        )
+        builder.synchronize(qubit)
+        builder.add(
+            CustomPulse(
+                qubit.get_cross_resonance_channel(self.model.get_qubit(1)),
+                samples=samples,
+            )
+        )
+        ir = PhysicalChannelAmplitudeValidation().run(builder)
+
+        assert ir == builder
+
+    def test_not_all_reset_by_sync(self):
+        # Create custom sine waveform pulse
+        t = np.linspace(0.0, 10 * np.pi, num=200)
+        samples = (np.sin(t) + 1j * np.cos(t)).tolist()
+        qubit0 = self.model.get_qubit(0)
+        qubit1 = self.model.get_qubit(1)
+        builder = self.model.create_builder()
+        builder.repeat(2000)
+        builder.add(
+            Pulse(
+                qubit0.get_drive_channel(),
+                PulseShapeType.SQUARE,
+                width=1e-06,
+                amp=0.9,
+                ignore_channel_scale=True,
+            )
+        )
+        builder.synchronize(
+            [
+                qubit0.get_cross_resonance_cancellation_channel(qubit1),
+                qubit1.get_cross_resonance_channel(qubit0),
+            ]
+        )
+        builder.add(
+            CustomPulse(
+                qubit0.get_cross_resonance_channel(qubit1),
+                samples=samples,
+            )
+        )
+        with pytest.raises(ValueError):
+            PhysicalChannelAmplitudeValidation().run(builder)
+
+    def test_reset_by_sync_fails(self):
+        # Create custom sine waveform pulse
+        model = EchoModelLoader(qubit_count=3).load()
+        qubit1 = model.get_qubit(0)
+        qubit2 = model.get_qubit(1)
+        qubit3 = model.get_qubit(2)
+        builder = model.create_builder()
+        builder.add(
+            Pulse(
+                qubit2.get_drive_channel(),
+                PulseShapeType.SQUARE,
+                width=1e-06,
+                amp=0.4,
+                ignore_channel_scale=True,
+            )
+        )
+        builder.add(
+            Pulse(
+                qubit2.get_cross_resonance_channel(qubit3),
+                PulseShapeType.SQUARE,
+                width=1e-06,
+                amp=0.4,
+                ignore_channel_scale=True,
+            )
+        )
+        builder.synchronize(
+            [qubit2.get_drive_channel(), qubit2.get_cross_resonance_channel(qubit3)]
+        )
+        builder.add(
+            Pulse(
+                qubit2.get_cross_resonance_cancellation_channel(qubit1),
+                PulseShapeType.SQUARE,
+                width=1e-06,
+                amp=0.4,
+                ignore_channel_scale=True,
+            )
+        )
+        with pytest.raises(ValueError):
+            PhysicalChannelAmplitudeValidation().run(builder)
