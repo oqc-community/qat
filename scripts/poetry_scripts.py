81b89456aeb2ede34c80f2bca9d236e3cacb191d
Kajsa <58070002+keriksson-rosenqvist-oqc@users.noreply.github.com>
2025-04-10 09:39:02 +0100

    Tooling for local docs building. (#157)
    GitOrigin-RevId: ae7acca9712c870504e93aaa9a18fa4c31373a31

--- scripts/poetry_scripts.py
+++ scripts/poetry_scripts.py
@@ -9,3 +9,17 @@ def format_code():
 
 def jupytext_sync():
     os.system("poetry run jupytext --sync --pipe black notebooks/ipynb/*.ipynb")
+
+
+def build_docs():
+    from importlib.util import find_spec
+
+    if find_spec("sphinx") is None:
+        print(
+            "Buliding docs requires the optional group 'docs' to be installed.\n"
+            "Please run\n\tpoetry install --with docs\nthen try again."
+        )
+        exit(1)
+    # Requires the package to have been installed with the --with docs flag
+    os.system("poetry run sphinx-apidoc -f -M -e -o docs/source/ src")
+    os.system("poetry run sphinx-build docs/source/ docs/build/")
