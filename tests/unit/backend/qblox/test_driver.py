f1fd8271a46739ce043c9795fc591d75a2046d37
Hamid <137192130+helmaazouz-oqc@users.noreply.github.com>
2025-08-19 14:46:57 +0100

    Bump up qblox-instruments to 0.17.1 (#400)
    GitOrigin-RevId: 259248bad4c2f34a45eca6a45698ff5ddd436541

--- tests/unit/backend/qblox/test_driver.py
+++ tests/unit/backend/qblox/test_driver.py
@@ -6,4 +6,4 @@ from importlib.metadata import version
 # TODO: Write tests to verify driver <-> fw compatibility. COMPILER-661
 class TestQbloxExecutable:
     def test_qiskit_instruments_compatibility(self):
-        assert version("qblox_instruments") == "0.16.0"
+        assert version("qblox_instruments") == "0.17.1"
