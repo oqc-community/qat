510e98f6e734886e8f3147d0e69add0f9524ad75
lcauser-oqc <lcauser@oxfordquantumcircuits.com>
2025-09-15 19:19:18 +0100

    [COMPILER-735] Add base class for exceptions caught at execution (#437)
    GitOrigin-RevId: 7ae1eb02cdb25ad4839ce7f1b5b51daae18f6778

---
+++ src/qat/runtime/exceptions.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Oxford Quantum Circuits Ltd
+
+
+class ExecutionException(Exception):
+    """Base class for exceptions for execution of programs in QAT."""
+
+    ...
